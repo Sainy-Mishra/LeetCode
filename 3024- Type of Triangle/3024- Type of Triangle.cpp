0de5a99419d149ff93d6ea7eaeaae19a669d48c2
Sainy-Mishra <sainymishra2004@gmail.com>
2025-05-21 16:48:00 +0530

    update

--- 3024- Type of Triangle/3024- Type of Triangle.cpp
+++ 3024- Type of Triangle/3024- Type of Triangle.cpp
@@ -1,16 +1,20 @@
 class Solution {
-public:
-    vector<int> productExceptSelf(vector<int>& nums) {
-        vector<int> answer;
-        for (int i = 0; i < nums.size(); i++) {
-            int prod = 1;
-            for (int j = 0; j < nums.size(); j++) {
-                if (i != j) {
-                    prod *= nums[j];
-                }
+    public:
+        string triangleType(vector<int>& nums) {
+            int a=nums[0];
+            int b=nums[1];
+            int c=nums[2];
+            if(a+b<=c || b+c<=a|| a+c<=b){
+                return("none");
             }
-            answer.push_back(prod);
+            if(a==b && b==c && c==a){
+                return("equilateral");
+            }
+            else if(((a==b) && (b!=c) && (a!=c)) || ((b==c) && (b!=a) && (c!=a)) || ((c==a) && (c!=b) && (a!=b))){
+                return("isosceles");
+            }
+            else{
+                return("scalene");
+            }
         }
-        return answer;
-    }
-};
+    };
