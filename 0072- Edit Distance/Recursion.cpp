13445e7f3196c2d1b850ee3337369357d6dc7d8e
Sainy-Mishra <sainymishra2004@gmail.com>
2025-07-18 12:29:35 +0530

    Problem 18/7

---
+++ 0072- Edit Distance/Recursion.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+
+    int distance(string s1, string s2, int i, int j){
+        if(i<0) return j+1;
+        if(j<0) return i+1;
+
+        if(s1[i]==s2[j]){
+            return distance(s1,s2,i-1,j-1);
+        }
+        return 1+min(distance(s1,s2,i,j-1), min(distance(s1,s2,i-1,j),distance(s1,s2,i-1,j-1)));
+    }
+
+    int minDistance(string word1, string word2) {
+        int m=word1.length();
+        int n=word2.length();
+        return distance(word1, word2, m, n);
+    }
+};
