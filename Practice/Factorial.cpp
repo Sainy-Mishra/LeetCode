dc22fcbd9a47d1643994845dd3fff65f8f610080
Sainy-Mishra <sainymishra2004@gmail.com>
2025-09-26 16:56:24 +0530

    Factorial

---
+++ Practice/Factorial.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    int fact=1;
+    cin>>n;
+    if(n<0){
+        cout<<"Error";
+    }
+    else{
+        for(int i=1;i<=n;i++){
+            fact=fact*i;
+        }
+        cout<<"Factorial of "<<n<<" is: "<<fact;
+    }
+    return 0;
+}
