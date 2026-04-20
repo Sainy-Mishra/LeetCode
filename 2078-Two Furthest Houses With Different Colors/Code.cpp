f47e4dabfe7fb10e7def57381367005bffa0a0b9
Sainy-Mishra <sainymishra2004@gmail.com>
2026-04-20 23:14:18 +0530

    POTD

---
+++ 2078-Two Furthest Houses With Different Colors/Code.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxDistance(vector<int>& colors) {
+        int result=0;
+
+        for(int i=0;i<colors.size();i++){
+            for(int j=i+1;j<colors.size();j++){
+                if(colors[i]!=colors[j]){
+                    result=max(result,abs(j-i));
+                }
+            }
+        }
+
+        return result;
+    }
+};
